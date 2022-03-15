efd0e6c04d39fd2614ef04df43129e68d8cb783d
Dawid Binkowski <d.k.binkowski@gmail.com>
2022-03-15 15:46:17 +0100

    Add README.md and some sounds in index.js

---
+++ README.md
@@ -0,0 +1,7 @@
+# 👨🏻‍💻Drum Kit Task from Web Development Bootcamp 2022👨🏻‍💻
+
+### `In this repository You can find very simple Drum Kit which I've built while studying Bootcamp `
+
+## You must only run index.html 👨🏻‍💻 to start a party🎉.
+
+### Visit dr Angela Yu bootcamp on this [Website](https://www.udemy.com/course/the-complete-web-development-bootcamp/)
--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+/////////////////ELEMENTS/////////////////////////////
 const w = document.querySelector(".w");
-const tom1Audio = document.querySelector(".tom1");
+const a = document.querySelector(".a");
+const s = document.querySelector(".s");
+const d = document.querySelector(".d");
+const j = document.querySelector(".j");
+const k = document.querySelector(".k");
+const l = document.querySelector(".l");
 
-function playSound(e) {
-  if (e.keyCode === 87) {
-    tom1Audio.play();
-  }
-}
+/////////////////SOUNDS///////////////////////////////
+const tom1Sound = new Audio("./src/sounds/tom-1.mp3");
+const tom2Sound = new Audio("./src/sounds/tom-2.mp3");
+const tom3Sound = new Audio("./src/sounds/tom-3.mp3");
+const tom4Sound = new Audio("./src/sounds/tom-4.mp3");
+const crash = new Audio("./src/sounds/crash.mp3");
+const kickBass = new Audio("./src/sounds/kick-bass.mp3");
+const snare = new Audio("./src/sounds/snare.mp3");
 
+/////////////////EVENT LISTENERS//////////////////////
 w.addEventListener("click", (e) => {
-  console.log("play");
-  if (e.keyCode === 87) {
-    console.log("button w click");
-  }
+	console.log("Play Tom1 when was click");
+	tom1Sound.play();
+});
+
+w.addEventListener("keyup", (e) => {
+	if (e.keyCode === 87) {
+		console.log("Button W was clicked");
+		tom1Sound.play();
+	}
+});
+
+a.addEventListener("click", (e) => {
+	console.log("Play Tom2 when was click");
+	tom2Sound.play();
+});
+
+a.addEventListener("keyup", (e) => {
+	if (e.keyCode === 65) {
+		console.log("Button A was clicked");
+		tom2Sound.play();
+	}
 });
