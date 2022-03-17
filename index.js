d01e518ff293a700155862079ef0fcf039cd6b51
Dawid Binkowski <d.k.binkowski@gmail.com>
2022-03-17 19:01:49 +0100

    changes in sounds and in functions

--- index.js
+++ index.js
@@ -1,99 +1,102 @@
 /////////////////ELEMENTS/////////////////////////////
-const w = document.querySelector(".w");
-const a = document.querySelector(".a");
-const s = document.querySelector(".s");
-const d = document.querySelector(".d");
-const j = document.querySelector(".j");
-const k = document.querySelector(".k");
-const l = document.querySelector(".l");
+const drums = [...document.querySelectorAll(".drum")];
 
 /////////////////SOUNDS///////////////////////////////
-const tom1Sound = new Audio("./src/sounds/tom-1.mp3");
-const tom2Sound = new Audio("./src/sounds/tom-2.mp3");
-const tom3Sound = new Audio("./src/sounds/tom-3.mp3");
-const tom4Sound = new Audio("./src/sounds/tom-4.mp3");
+const tom1Sound = new Audio("./src/sounds/tom1.mp3");
+const tom2Sound = new Audio("./src/sounds/tom2.mp3");
+const tom3Sound = new Audio("./src/sounds/tom3.mp3");
+const tom4Sound = new Audio("./src/sounds/tom4.mp3");
 const crash = new Audio("./src/sounds/crash.mp3");
-const kickBass = new Audio("./src/sounds/kick-bass.mp3");
+const kickBass = new Audio("./src/sounds/kick.mp3");
 const snare = new Audio("./src/sounds/snare.mp3");
 
 /////////////////EVENT LISTENERS//////////////////////
-w.addEventListener("click", (e) => {
-	console.log("Play Tom1 when was click");
-	tom1Sound.play();
-});
 
-w.addEventListener("keydown", (e) => {
-	if (e.keyCode === 87) {
-		console.log("Button W was clicked");
+for (let i = 0; i < drums.length; i++) {
+	drums[i].addEventListener("click", function (e) {
 		tom1Sound.play();
-	}
-});
+		console.log(this.innerHTML);
+		console.log();
+	});
+}
 
-a.addEventListener("click", (e) => {
-	console.log("Play Tom2 when was click");
-	tom2Sound.play();
-});
+// w.addEventListener("click", (e) => {
+// 	console.log("Play Tom1 when was click");
+// 	tom1Sound.play();
+// });
 
-a.addEventListener("keydown", (e) => {
-	if (e.keyCode === 65) {
-		console.log("Button A was clicked");
-		tom2Sound.play();
-	}
-});
+// w.addEventListener("keydown", (e) => {
+// 	if (e.keyCode === 87) {
+// 		console.log("Button W was clicked");
+// 		tom1Sound.play();
+// 	}
+// });
 
-s.addEventListener("click", (e) => {
-	console.log("Play Tom3 when was click");
-	tom3Sound.play();
-});
+// a.addEventListener("click", (e) => {
+// 	console.log("Play Tom2 when was click");
+// 	tom2Sound.play();
+// });
 
-s.addEventListener("keydown", (e) => {
-	if (e.keyCode === 83) {
-		console.log("Button S was clicked");
-		tom3Sound.play();
-	}
-});
+// a.addEventListener("keydown", (e) => {
+// 	if (e.keyCode === 65) {
+// 		console.log("Button A was clicked");
+// 		tom2Sound.play();
+// 	}
+// });
 
-d.addEventListener("click", (e) => {
-	console.log("Play Tom4 when was click");
-	tom4Sound.play();
-});
+// s.addEventListener("click", (e) => {
+// 	console.log("Play Tom3 when was click");
+// 	tom3Sound.play();
+// });
 
-d.addEventListener("keydown", (e) => {
-	if (e.keyCode === 68) {
-		console.log("Button S was clicked");
-		tom4Sound.play();
-	}
-});
-j.addEventListener("click", (e) => {
-	console.log("Play Crash when was click");
-	crash.play();
-});
+// s.addEventListener("keydown", (e) => {
+// 	if (e.keyCode === 83) {
+// 		console.log("Button S was clicked");
+// 		tom3Sound.play();
+// 	}
+// });
 
-j.addEventListener("keydown", (e) => {
-	if (e.keyCode === 74) {
-		console.log("Button J was clicked");
-		crash.play();
-	}
-});
-k.addEventListener("click", (e) => {
-	console.log("Play Kick-bass when was click");
-	kickBass.play();
-});
+// d.addEventListener("click", (e) => {
+// 	console.log("Play Tom4 when was click");
+// 	tom4Sound.play();
+// });
 
-k.addEventListener("keydown", (e) => {
-	if (e.keyCode === 75) {
-		console.log("Button K was clicked");
-		kickBass.play();
-	}
-});
-l.addEventListener("click", (e) => {
-	console.log("Play Snare when was click");
-	snare.play();
-});
+// d.addEventListener("keydown", (e) => {
+// 	if (e.keyCode === 68) {
+// 		console.log("Button S was clicked");
+// 		tom4Sound.play();
+// 	}
+// });
+// j.addEventListener("click", (e) => {
+// 	console.log("Play Crash when was click");
+// 	crash.play();
+// });
 
-l.addEventListener("keydown", (e) => {
-	if (e.keyCode === 76) {
-		console.log("Button L was clicked");
-		snare.play();
-	}
-});
+// j.addEventListener("keydown", (e) => {
+// 	if (e.keyCode === 74) {
+// 		console.log("Button J was clicked");
+// 		crash.play();
+// 	}
+// });
+// k.addEventListener("click", (e) => {
+// 	console.log("Play Kick-bass when was click");
+// 	kickBass.play();
+// });
+
+// k.addEventListener("keydown", (e) => {
+// 	if (e.keyCode === 75) {
+// 		console.log("Button K was clicked");
+// 		kickBass.play();
+// 	}
+// });
+// l.addEventListener("click", (e) => {
+// 	console.log("Play Snare when was click");
+// 	snare.play();
+// });
+
+// l.addEventListener("keydown", (e) => {
+// 	if (e.keyCode === 76) {
+// 		console.log("Button L was clicked");
+// 		snare.play();
+// 	}
+// });
